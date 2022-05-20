9b1207790d00fc091a794c661dc9552c1dc7e94b
Shay Ben Simon <shay.bs@spot.im>
2022-05-20 13:15:28 +0300

    message handlers

--- server/src/services/message-handlers.js
+++ server/src/services/message-handlers.js
@@ -1,12 +1,28 @@
 const { CHANNEL_NAME } = require('./constants');
 
 const ioMessageHandler = (io, socket) => {
-  socket.on(CHANNEL_NAME, (msg) => {
-    if (msg.text.search(new RegExp('Elon', 'i')) !== -1) {
-      io.to(socket.id).emit(CHANNEL_NAME, { ...msg, text: 'message to elon' });
+  socket.on(CHANNEL_NAME, (message) => {
+    if (message.text.search(new RegExp('Elon', 'i')) !== -1) {
+      sendPrivateMessage({ io, socket, text: 'message from elon' });
     } else {
-      io.emit(CHANNEL_NAME, { ...msg, text: 'broadcast' });
+      broadcastMessage({ io, message });
     }
+  });
+};
+
+const sendPrivateMessage = ({ io, socket, text }) => {
+  io.to(socket.id).emit(CHANNEL_NAME, {
+    username: 'Elon',
+    timeStamp: new Date().toLocaleTimeString('it-IT'),
+    text
+  });
+};
+
+const broadcastMessage = ({ io, socket, message }) => {
+  io.emit(CHANNEL_NAME, {
+    ...message,
+    sessionId: socket.id,
+    text: 'broadcast'
   });
 };
 
